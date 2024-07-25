adfbcaa577edde3d1667f7b2adc6730863145ae6
Faruk Cagan <faruk.cagan@bestera.com.tr>
2024-07-25 13:47:19 +0300

    build error fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
+import AuthModal from "@/components/Auth/AuthScreen";
 import Content from "@/components/Content/Content";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
-import dynamic from "next/dynamic";
 
-const AuthModal = dynamic(() => import("@/pages/Auth/AuthScreen"), {
-  ssr: false,
-});
 
 export default function Home() {
   return (
